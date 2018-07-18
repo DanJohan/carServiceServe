0b8f8b37f4a11da2d9095d0a795ce8e396b7f9ad
DanJohan <bestbuysystems01@gmail.com>
2018-07-18 17:32:44 +0530

    Initial commit

---
+++ app/src/main/java/com/car/carserviceserve/EnquiryFormActivity.java
@@ -0,0 +1,13 @@
+package com.car.carserviceserve;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EnquiryFormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enquiry_form);
+    }
+}
